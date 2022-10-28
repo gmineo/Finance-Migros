3241ee6eabbc240c3083587467fe46c2cf7d67a2
Gmineo <39634149+gmineo@users.noreply.github.com>
2022-10-28 11:09:43 +0200

    Update stock_price_web_application.py

--- stock_price_web_application.py
+++ stock_price_web_application.py
@@ -115,7 +115,7 @@ for line in r.iter_lines():
         sig=segnale_xcu(price_ini,price)
         delta_price= ((price/price_ini)-1)*100
         st.write(time, price,  ' --->           ', sig , '             delta: ', delta_price, end='')
-        st.markdown(f'<h1 style="color:#008400;font-size:15px;">{sig}</h1>,<h1 style="color:#008400;font-size:15px;">{sig}</h1>', unsafe_allow_html=True)
+        st.markdown(f'{time}, <h1 style="color:#008400;font-size:15px;">{sig}</h1>', unsafe_allow_html=True)
         
   except:
     pass # doing nothing on exception
